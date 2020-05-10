6fb96ed521b0099b8ee87931672d40800243266e
GeorgeWL <george.langham@ivendi.com>
2020-05-10 18:30:53 +0100

    add theme handlers

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,15 +5,16 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
-
+import ThemeContext from "../context/themeContext"
+const Themes = { light: 'light-theme', dark: 'dark-theme' }
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+	const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -22,30 +23,40 @@ const Layout = ({ children }) => {
       }
     }
   `)
+	const [theme, setTheme] = useState(Themes.light)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
+	return (
+		<div className={`layout-${theme}`}>
+			<Header
+				siteTitle={data.site.siteMetadata.title}
+				theme={theme}
+				changeTheme={() => {
+					setTheme(prev => prev === Themes.light ?
+						Themes.dark :
+						Themes.light)
+				}} />
+			<div
+				style={{
+					margin: `0 auto`,
+					maxWidth: 960,
+					padding: `0 1.0875rem 1.45rem`,
+				}}
+			>
+				<main>{children}</main>
+				<footer>
+					© {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+					<a href="https://www.gatsbyjs.org">Gatsby</a>
+				</footer>
+			</div>
+		</div>
+	)
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 }
+
+Layout.contextType = ThemeContext;
 
 export default Layout
