eb159eba1040a981a9b32d7fdd87145a2ec8310c
GeorgeWL <george.langham@ivendi.com>
2020-05-10 19:29:51 +0100

    themeswitch props

--- src/components/theme-switch.js
+++ src/components/theme-switch.js
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './themeSwitch.scss';
 import Icon from './icon';
-const ThemeSwitch = ({ theme, changeTheme }) => (
+const ThemeSwitch = ({ theme, onChange }) => (
 	<div className='themeSwitch'>
 		<label htmlFor='light-theme'>Light</label>
-		<Icon name='sun' height='50px' width='50px' />
+		<Icon name='sun' height='50px' width='50px' onChange={onChange} />
 		<input type='radio' name='theme' value='light' id='light-theme' />
-		<input type='radio' name='theme' value='dark' id='dark-theme' />
+		<input type='radio' name='theme' value='dark' id='dark-theme' onChange={onChange} />
 		<label htmlFor='dark-theme'>Dark</label>
 		<Icon name='moon' height='50px' width='50px' />
 	</div>
