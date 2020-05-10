ff344b6e4ce9259a21feddfc249fc6773a35f4b3
GeorgeWL <george.langham@ivendi.com>
2020-05-10 18:31:22 +0100

    boilerplate for icon

---
+++ src/components/icon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './icon.scss';
+const Icon = ({ Props }) => (
+	<div className='icon'>
+
+	</div>
+);
+Icon.defaultProps = {
+
+};
+Icon.propTypes = {
+
+};
+export default Icon;
